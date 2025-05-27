d66cb94f58e992837b2b60a29c7e45cb1a0c0499
schlotz2003 <noah.schlotz@gmx.de>
2025-05-27 12:01:57 +0200

    Add .gitignore and example env file

--- configuration/config.py
+++ configuration/config.py
@@ -1,8 +1,11 @@
 import google.generativeai as genai
 from elevenlabs import ElevenLabs, save
+from dotenv import load_dotenv
+import os
 
-# === 🗝️ API-Keys ===
-GOOGLE_TRANSLATE_API_KEY = "AIzaSyDY21rw19lxCvjlC-Iy8etmRmqJop_pvR8"  # Für Google Translate
-GEMINI_API_KEY = "AIzaSyBZQN8kt4d7BqweJ2OLrjtNBKtG1aiy9S0"  # Für Gemini (LLM)
-GOOGLE_TTS_API_KEY = "AIzaSyBC_55hmGUFTCbELW4-PfeKljaOHCtVmKw" # 🔊 Für Google Text-to-Speech (bitte hier einfügen)
-ELEVENLABS_API_KEY= "sk_3bbeb7253a4230cecb38cca2146bc7e47abc2a1f2f0001f4" # Key für ElevenLabs AI für text to speech funktion.
+load_dotenv(dotenv_path="configuration/config.env")
+
+GOOGLE_TRANSLATE_API_KEY = os.getenv("GOOGLE_TRANSLATE_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GOOGLE_TTS_API_KEY = os.getenv("GOOGLE_TTS_API_KEY")
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
